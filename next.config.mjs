93f833e63e4483aecd3af83148e7c4eb1cbdd0b4
Faklor <faklor_br_ar@mail.ru>
2025-02-09 22:18:51 +0300

    delBasePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
     output: 'export',
-    basePath: '/love-day',
-    assetPrefix: '/love-day',
     images: {
       unoptimized: true,
     },
