5bdf1f0446f88a85fb10261b4e8e6670184c121a
Faklor <faklor_br_ar@mail.ru>
2025-02-09 22:37:56 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
     output: 'export',
-    basePath:'/love-day/',
+    basePath:'/love-day',
     images: {
       unoptimized: true,
     },
