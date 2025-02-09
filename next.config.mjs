b6b3a31f4fa8bb204f08c61db65c36b6d4b37c96
Faklor <faklor_br_ar@mail.ru>
2025-02-09 21:50:47 +0300

    configEdit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { 
+    output: 'export', // Статическая сборка для GitHub Pages
+    images: {
+      unoptimized: true, // Для работы с GitHub Pages
+    },
+    // Отключаем телеметрию, если хотите
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+};
 
 export default nextConfig;
